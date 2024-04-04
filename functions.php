ab6e1453e64fa58d0df5bf006e8791d62111b760
Jerome Audiard <jerome.audiard@gmail.com>
2024-04-04 15:17:51 +0200

    Add hero banner

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 //  Ajouter CSS + JS
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles(){
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 	wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.min.css'));
 	wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/burger.js', array(), filemtime(get_stylesheet_directory() . '/js/burger.js'), true );
 }
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
-
+<div class="containerHero">
+    <h1>Photographe event</h1>
+</div>
 <h1>Test H1</h1>
 <h2>Test H2</h2>
 <h3>Test H3</h3>
